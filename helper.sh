15a7ade5f62ac40bedb139a688573073cc9b98bb
Sjoerd Santema <Sjoerd Santema>
2018-09-12 11:07:17 +0200

    added the complete helper script

--- helper.sh
+++ helper.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
+clear
+. helper.conf 
 echo " "
 cat sentia.txt
 echo " "
+echo "Stack we're working on is:" $stack
+echo "The bucket we're getting our templates from and to is:" $bucket
+echo "We're loading this template when updating:" $template  
+echo "--------------------------------------------------------------------------------------------"
 echo " "
 echo "What would you like to do? Choices are: sync, delete, create or quit:" 
 read wish
 
 if [ "$wish" == "create" ] ; then
         echo "As you wish, we shall create a new stack."
-        aws cloudformation create-stack --stack-name 'Big-Donkey' --template-url 'https://s3.us-east-2.amazonaws.com/big-donkey/parent-stack.yaml' --capabilities CAPABILITY_IAM 
-        sleep 10
+        aws cloudformation create-stack --stack-name $stack --template-url $template --capabilities CAPABILITY_IAM 
+        sleep 5
         clear
         ./helper.sh 
         exit 1
 elif [ "$wish" == "sync" ] ; then
         echo " "
-        aws s3 sync . s3://big-donkey --exclude '*' --include '*.yaml' 
-        aws cloudformation update-stack --stack-name 'Big-Donkey' --template-url 'https://s3.us-east-2.amazonaws.com/big-donkey/parent-stack.yaml' 
-        echo "We shall bring the news to every corner and s3 bucket"
-        sleep 10
+        aws s3 sync . $bucket --exclude '*' --include '*.yaml' 
+        aws cloudformation update-stack --stack-name $stack --template-url $template 
+        echo "We shall bring the news to every corner and s3 bucket."
+        sleep 5
         clear
         ./helper.sh 
         exit 1
 elif [ "$wish" == "delete" ] ; then
-        echo "We shall destroy what you have created" 
-        aws cloudformation delete-stack --stack-name 'Big-Donkey'
-        sleep 10
+        echo "We shall destroy what you have created." 
+        aws cloudformation delete-stack --stack-name $stack
+        sleep 5
         clear
         ./helper.sh  
         exit 1
