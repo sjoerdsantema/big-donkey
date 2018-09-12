d15148b0ed9fa7fe1a7507c6da360c52b05a4aed
Sjoerd Santema <Sjoerd Santema>
2018-09-12 10:47:37 +0200

    deleted old sync.sh

--- sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo syncing s3, perform cfn update and push to git repo
-aws s3 sync . s3://big-donkey --exclude '*' --include '*.yaml' &&
-aws cloudformation update-stack --stack-name 'Big-Donkey' --template-url 'https://s3.us-east-2.amazonaws.com/big-donkey/parent-stack.yaml' --capabilities CAPABILITY_IAM
-git add *
-git commit * -m "scripted commit"
-git push origin master
